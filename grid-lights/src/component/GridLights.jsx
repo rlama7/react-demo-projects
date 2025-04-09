aca7e623be3553286bcea907951598d5473c5876
Ratna Lama <royalray7@gmail.com>
2025-04-08 17:37:39 -0700

    add styles

--- grid-lights/src/component/GridLights.jsx
+++ grid-lights/src/component/GridLights.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import './css/style.css';
+
 const GridLights = () => {
-  return <div>GridLights</div>;
+  return (
+    <div className="grid-lights">
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
+        <div className="grid-lights-box" id={index}>
+          {item}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default GridLights;
